7d11b23e1457e27f3e391f62890166493d71b6c5
stefangrage <hej@stefangrage.dk>
2024-09-06 14:03:18 +0200

    Opdaterer astro og tilføjer Image assets i index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
 });
